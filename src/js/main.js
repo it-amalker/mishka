6ec451ff96435ee9b4a5b0f63010ef09ce0981f0
amalker <it-amalker@yandex.ru>
2019-12-06 13:55:53 +0300

    added menu open/close script

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
+const mainMenu = document.querySelector('.main-menu');
+const userMenu = document.querySelector('.user-menu');
+const openMenuButton = document.querySelector('.nav-toggle-off');
+const closeMenuButton = document.querySelector('.nav-toggle-on');
 
+
+openMenuButton.addEventListener('click', function () {
+  mainMenu.classList.add('main-menu--active');
+  userMenu.classList.add('user-menu--active');
+  openMenuButton.classList.remove('nav-toggle-off--active');
+  closeMenuButton.classList.add('nav-toggle-on--active');
+})
+
+closeMenuButton.addEventListener('click', function () {
+  mainMenu.classList.remove('main-menu--active');
+  userMenu.classList.remove('user-menu--active');
+  closeMenuButton.classList.remove('nav-toggle-on--active');
+  openMenuButton.classList.add('nav-toggle-off--active');
+})
