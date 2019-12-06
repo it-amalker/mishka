65a1816e1ec866ecbd4d6f4739f54e908a398d09
amalker <it-amalker@yandex.ru>
2019-12-06 18:00:51 +0300

    rewrite menu open/close behavior, refactor js code

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,42 @@
 const mainMenu = document.querySelector('.main-menu');
 const userMenu = document.querySelector('.user-menu');
 const openMenuButton = document.querySelector('.nav-toggle-off');
-const closeMenuButton = document.querySelector('.nav-toggle-on');
+
 const popup = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 
-// Opening main menu
-openMenuButton.addEventListener('click', function () {
-  mainMenu.classList.add('main-menu--active');
-  userMenu.classList.add('user-menu--active');
-  openMenuButton.classList.remove('nav-toggle-off--active');
-  closeMenuButton.classList.add('nav-toggle-on--active');
-})
 
-// Closing main menu
-closeMenuButton.addEventListener('click', function () {
-  mainMenu.classList.remove('main-menu--active');
-  userMenu.classList.remove('user-menu--active');
-  closeMenuButton.classList.remove('nav-toggle-on--active');
-  openMenuButton.classList.add('nav-toggle-off--active');
-})
+// Opening and closing main menu
+
+document.querySelector('.nav-toggle').addEventListener('click', (evt) => {
+  mainMenu.classList.toggle('main-menu--active');
+  userMenu.classList.toggle('user-menu--active');
+  evt.target.classList.toggle('nav-toggle--close');
+});
 
 
-// Opening popup to choose goods sizes, an overlay will appear
-document.querySelector('.bestseller__order-button').addEventListener('click', function () {
+// Opening popup for choosing goods sizes, an overlay will appear
+
+document.querySelector('.bestseller__order-button').addEventListener('click', () => {
   popup.classList.add('modal--active');
   overlay.classList.add('overlay--active');
 });
 
+
 // Closing popup when clicking on the overlay
 
-document.querySelector('.overlay').addEventListener('click', function () {
+document.querySelector('.overlay').addEventListener('click', () => {
   popup.classList.remove('modal--active');
   overlay.classList.remove('overlay--active');
 });
 
+
 // Preventing close popup
 
-popup.addEventListener("click", function (evt) {
+popup.addEventListener("click", (evt) => {
   evt.stopPropagation();
 });
+
 
 // Selecting certain size
 
